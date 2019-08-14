9e26efda615e2a909b25ce7e5574f8c776f78fd4
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-08-14 17:10:12 +0530

    collection set

--- java/14-08-19/src/collection/CollectionDemo.java
+++ java/14-08-19/src/collection/CollectionDemo.java
@@ -7,22 +7,20 @@ import java.util.Set;
 public class CollectionDemo {
 	private String str[];
 	private static Scanner sc = new Scanner(System.in); 
-	private Set<String> set;
+	private Set<String> set = new HashSet<String>();
 	public CollectionDemo() {
 		System.out.println("enter the number of elements: ");
 		str = new String[sc.nextInt()];
 		for(int i = 0; i<str.length ; i++) {
 			System.out.println(i+"th element: ");
 			str[i] = sc.next().toString().toLowerCase();
-		}
-		
-		set = new HashSet<String>();
-		for(String s : str) {
-			if(!set.add(s)) {
-				System.out.println("duplicate element found---->  " + s);
+			if(!set.add(str[i])) {
+				System.err.println("duplicate element found---->  " + str[i]);
+				
 			}
 		}
 		
+		System.out.println("Elements in set are: ");
 		for(String s: set) {
 			System.out.println(s);
 		}
