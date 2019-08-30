c8df0ae9d4b784bda6c8c5fc71a2f2a20a02d743
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-08-30 22:11:20 +0530

    maven-crud

---
+++ java/12_08_2019_FSD/src/abstract_class/a.java
@@ -0,0 +1,9 @@
+package abstract_class;
+
+public class a {
+
+	public void process() throws Exception{
+		System.out.println("a");
+	}
+
+}
---
+++ java/12_08_2019_FSD/src/abstract_class/b.java
@@ -0,0 +1,14 @@
+package abstract_class;
+
+public class b{
+
+	public String count() {
+		return "a";	}
+	  public static void main( String[] args) {
+		  new b().count();
+		  
+	  }
+
+}
+
+
