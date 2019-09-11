fd7169197c139e8c7cb5ed2ad2542361206703d9
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-09-11 21:12:35 +0530

    add league

--- spring/spring-addLeague/src/main/java/com/example/spring/controller/HomeController.java
+++ spring/spring-addLeague/src/main/java/com/example/spring/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.spring.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.spring.model.League;
 import com.example.spring.repository.LeagueRepository;
@@ -23,7 +26,7 @@ public class HomeController {
 	
 	@GetMapping("/list")
 	public String showList(Model theModel) {
-		theModel.addAttribute("list",leagueRepository.findAll());
+		theModel.addAttribute("list",leagueRepository.findAllByOrderByTitleAsc());
 		
 		return "index";
 		
@@ -45,5 +48,37 @@ public class HomeController {
 		
 		return "redirect:/league/list";
 		
+	}
+	
+	@PostMapping("/deleteLeague")
+	public String deleteLeague(@RequestParam("id") int id) {
+		leagueRepository.deleteById(id);
+		
+		return "redirect:/league/list";
+		
+	}
+	@PostMapping("/updateLeague")
+	public String updateLeague(@RequestParam("id") int id, Model theModel) {
+		Optional<League> result = leagueRepository.findById(id);
+		League league = result.get();
+		theModel.addAttribute("league" , league);
+		String[] season = {"summer","winter","spring","rainy" ,"ihh"};
+		theModel.addAttribute("season",season);
+		leagueRepository.deleteById(id);
+		
+		return "add-form";
+		
+	}
+	
+	@PostMapping("/searchLeague")
+	public String searchLeague(@RequestParam("search") String search, Model theModel) {
+		if(search.length()==0) {
+			theModel.addAttribute("list",leagueRepository.findAllByOrderByTitleAsc());
+			return "index";
+		}
+		else {
+		theModel.addAttribute("list",leagueRepository.findAllByTitleContainingIgnoreCaseOrSeasonContainingIgnoreCase(search, search));
+		return "index";
+		}
 	}
 }
--- spring/spring-addLeague/src/main/java/com/example/spring/repository/LeagueRepository.java
+++ spring/spring-addLeague/src/main/java/com/example/spring/repository/LeagueRepository.java
@@ -1,5 +1,7 @@
 package com.example.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.spring.model.League;
 
 @Repository
 public interface LeagueRepository extends CrudRepository<League, Integer> {
-
+	public List<League> findAllByOrderByTitleAsc(); 
+	public List<League> findAllByTitleContainingIgnoreCaseOrSeasonContainingIgnoreCase(String title,String season);
 }
