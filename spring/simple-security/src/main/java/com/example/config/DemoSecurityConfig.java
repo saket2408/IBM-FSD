84a1066b9abf740f295b9a5656bc2783d664bb5e
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-09-12 16:11:16 +0530

    spring security

---
+++ spring/simple-security/src/main/java/com/example/config/DemoSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+			.withUser(users.username("saket").password("pass@123").roles("EMPLOYEE"))
+			.withUser(users.username("admin").password("admin").roles("ADMIN"))
+			.withUser(users.username("root").password("root").roles("MANAGER"));
+	}
+
+	
+}
