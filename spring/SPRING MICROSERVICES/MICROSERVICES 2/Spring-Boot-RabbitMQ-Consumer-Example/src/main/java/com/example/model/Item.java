84ed3430e9fa6842cf8a4aa36e05bbfe60965341
b4ibmjava08@iiht.tech <b4ibmjava08@iiht.tech>
2019-09-23 20:33:43 +0530

    spring-final

---
+++ spring/SPRING MICROSERVICES/MICROSERVICES 2/Spring-Boot-RabbitMQ-Consumer-Example/src/main/java/com/example/model/Item.java
@@ -0,0 +1,65 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class,property = "@id",scope = Item.class)
+
+public class Item {
+    private String itemName;
+    private String category;
+    private String description;
+    
+    
+	public Item() {
+		super();
+	}
+
+
+	public Item(String itemName, String category, String description) {
+		super();
+		this.itemName = itemName;
+		this.category = category;
+		this.description = description;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Item [itemName=" + itemName + ", category=" + category + ", description=" + description + "]";
+	}
+    
+}
